f7e8c59c920afa1e8eda3ce6a0bfdbb7bca908a5
USMAN AHMAD <usmaneng220@gmail.com>
2026-05-25 01:55:02 +0500

    Change HTML lang attribute class to 'dark'

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="bg-background">
+    <html lang="en" className="dark">
       <body
         className={`${inter.variable} ${jetbrainsMono.variable} font-sans antialiased`}
       >
