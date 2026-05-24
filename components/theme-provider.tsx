1d2223d3aac2ecca8ad871112fef3f24e9b3361c
USMAN AHMAD <usmaneng220@gmail.com>
2026-05-24 23:52:04 +0500

    added all radix ui dependencies and fixed types

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -3,7 +3,6 @@
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 
-// Hum props ko manually handle karenge taake TypeScript error na de
 export function ThemeProvider({ children, ...props }: any) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
